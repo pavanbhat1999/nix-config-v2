c64c965b3aa51a663b312c9562c3ed75cc212acc
pavanbhat1999 <prbhat07@gmail.com>
2023-10-12 22:56:48 +0530

    update reddit not working

--- configuration.nix
+++ configuration.nix
@@ -108,17 +108,17 @@
   # services.devmon.enable = true;
 
   # Enable the Plasma 5 Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   # services.xserver.displayManager.lightdm.enable = true;
   # services.xserver.displayManager.startx.enable = true;
   # services.xserver = {
   #     displayManager = {
   #       defaultSession = "hyprland";
   #       # defaultSession = "plasmawayland";
-  #       autoLogin = {
-  #         enable = true;
-  #         user = "root99";
-  #       };
+  #       # autoLogin = {
+  #       #   enable = true;
+  #       #   user = "root99";
+  #       # };
   #     };
   # };
   services.xserver.desktopManager.plasma5.enable = true;
