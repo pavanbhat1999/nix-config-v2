74aabd53ed559734f772e7ff836bf5de49620c64
pavanbhat1999 <prbhat07@gmail.com>
2023-10-22 12:36:40 +0530

    waybar custom package changed

--- home.nix
+++ home.nix
@@ -204,15 +204,15 @@ gtk.cursorTheme.name = "Bibata-Modern-Amber";
   #   enable = true;
   #   initExtra = "export PATH=/home/root99/bin/scripts:$PATH";
   # };
-programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
-    mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
-    patches = (oa.patches or [ ]) ++ [
-      (pkgs.fetchpatch {
-        name = "fix waybar hyprctl";
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
-        sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
-      })
-    ];
-  });
+# programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
+#     mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
+#     patches = (oa.patches or [ ]) ++ [
+#       (pkgs.fetchpatch {
+#         name = "fix waybar hyprctl";
+#         url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
+#         sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
+#       })
+#     ];
+#   });
 programs.home-manager.enable = true;
 }
