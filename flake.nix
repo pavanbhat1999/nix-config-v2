5c299ee51a28103d730a42ab4b09de7f05c6293e
pavanbhat1999 <prbhat07@gmail.com>
2023-11-17 07:53:56 +0530

    flake updated - unstable

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    ags.url = "github:Aylur/ags";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
