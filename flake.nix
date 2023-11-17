e12c518b647f93d4eed29691ab6a1a79264b4abf
pavanbhat1999 <prbhat07@gmail.com>
2023-11-17 21:46:34 +0530

    flake update

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     ags.url = "github:Aylur/ags";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager,ags ... }: {
+  outputs = inputs@{ nixpkgs, home-manager,ags, ... }: {
     nixosConfigurations = {
       root99 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
